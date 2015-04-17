ae394d6f170819a7aa6c2c5a8c45255b935e48c8
刘博楠 <liubonan@liubonandeMacBook-Pro.local>
2015-04-17 15:25:10 +0800

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by 刘博楠 on 15/4/17.
+//  Copyright (c) 2015年 刘博楠. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
